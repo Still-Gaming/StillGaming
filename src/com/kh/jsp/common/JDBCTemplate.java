20149df069efeee5feec20cbf89fb00df18725ea
User <User@121.132.112.123>
2020-11-03 16:33:15 +0900

    2020-11-03 16:32 commit: jsp.common and lib

---
+++ src/com/kh/jsp/common/JDBCTemplate.java
@@ -0,0 +1,61 @@
+package com.kh.jsp.common;
+
+import java.sql.*;
+
+public class JDBCTemplate {
+	public static Connection getConnection() {
+		Connection con = null;
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:xe", "STILL", "1234");
+			
+			con.setAutoCommit(false);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return con;
+	}
+	
+	public static void close(Statement stmt) {
+		try {
+			if(!stmt.isClosed()) stmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void close(ResultSet rset) {
+		try {
+			if(!rset.isClosed()) rset.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void close(Connection con) {
+		try {
+			if(!con.isClosed()) con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void commit(Connection con) {
+		try {
+			con.commit();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void rollback(Connection con) {
+		try {
+			con.rollback();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
